f96e6611b21cea952d9bfe8186a6a885ede075e9
Antony <antony.jacques-ext@gl-events.com>
2024-09-09 19:09:18 +0200

    wip single muscle view

--- src/views/SingleBodyPartView.vue
+++ src/views/SingleBodyPartView.vue
@@ -1,33 +1,51 @@
 <template>
-  <h2>All {{ targetedMuscle }} exercises</h2>
-  <p v-for="exercice in getSingleBodyPartExercices" :key="exercice.name" > {{ exercice.name }}</p>
+  <div class="container">
+    <h2 class="mb-8">All {{ targetedMuscle }} exercises</h2>
+    <div class="grid grid-cols-3 gap-x-4 gap-y-4">
+      <div
+        v-for="exercice in getSingleBodyPartExercices"
+        :key="exercice.id"
+        class="shadow-xl bg-cyan-800"
+      >
+        <div><img :src="exercice.gifUrl" alt="" class="w-full" /></div>
+        <div class="flex justify-end p-4">
+          <span
+            class="p-[0.75rem] ml-4 border rounded-full border-[#ecb101] text-[#ecb101] transition ease-in-out delay-150 hover:bg-[#ecb101] hover:text-white"
+            v-for="secondaryMuscle in exercice.secondaryMuscles"
+            :key="secondaryMuscle"
+            >{{ secondaryMuscle }}</span
+          >
+        </div>
+        <p class="p-4 first-letter:uppercase">
+          {{ exercice.name }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { computed, onMounted } from "vue";
 
-import { storeToRefs } from 'pinia'
-import { useBodyPartsStore } from '@/stores/bodyParts'
-
+import { useBodyPartsStore } from "@/stores/bodyParts";
 
 export default {
   props: ["bodyPart"],
-  setup(props){
+  setup(props) {
+    onMounted(() => {
+      store.fetchSingleBodyPartExercices(props.bodyPart);
+    });
 
-    onMounted(()=>{
-      store.fetchSingleBodyPartExercices(props.bodyPart)
-    })
-    
-    const test = props.bodyPart
-    const targetedMuscle = props.bodyPart
-    const store = useBodyPartsStore()
-    const getSingleBodyPartExercices = computed(()=> store.getSingleBodyPartExercices(targetedMuscle))
-
-    return { targetedMuscle, getSingleBodyPartExercices }
-  }
-}
+    const test = props.bodyPart;
+    const targetedMuscle = props.bodyPart;
+    const store = useBodyPartsStore();
+    const getSingleBodyPartExercices = computed(() =>
+      store.getSingleBodyPartExercices(targetedMuscle)
+    );
+    console.log("getSingleBodyPartExercices", getSingleBodyPartExercices);
+    return { targetedMuscle, getSingleBodyPartExercices };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>

3bf2936e99797a2445a4c76add00fe0d15c33c11
Antony <antony.jacques-ext@gl-events.com>
2024-09-09 19:29:29 +0200

    TargetView with setup

--- src/views/TargetView.vue
+++ src/views/TargetView.vue
@@ -1,35 +1,29 @@
 <template>
   <div class="container">
     <div class="grid grid-cols-3 gap-x-4 gap-y-4">
-      
       <div v-for="item in getBodyParts" :key="item">
-        <router-link :to="{name: 'SingleBodyPartView', params: {bodyPart: item}}">
-          <BodyPartPreview :bodyPart="item"/>
+        <router-link
+          :to="{ name: 'SingleBodyPartView', params: { bodyPart: item } }"
+        >
+          <BodyPartPreview :bodyPart="item" />
         </router-link>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { computed, defineComponent, onMounted } from "vue";
-import { useBodyPartsStore } from '@/stores/bodyParts'
+import { useBodyPartsStore } from "@/stores/bodyParts";
 import BodyPartPreview from "../components/BodyPartPreview.vue";
 
-export default defineComponent({
-  name: "TargetView",
-  components: { BodyPartPreview },
-  setup() {
+const store = useBodyPartsStore();
+const getBodyParts = computed(() => {
+  return store.getBodyParts;
+});
 
-    const store = useBodyPartsStore()
-    const getBodyParts = computed(()=>{ return store.getBodyParts }) 
-
-    onMounted(()=>{
-      store.fetchBodyParts()
-    })
-
-    return { getBodyParts };
-  },
+onMounted(() => {
+  store.fetchBodyParts();
 });
 </script>
 

98694cf6cf8fb8c3765bbd029b9d4c4b9b0a690d
Antony <antony.jacques-ext@gl-events.com>
2024-10-04 08:51:20 +0200

    ExerciseItemPreview

---
+++ src/components/ExerciseItemPanel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-white p-4">
+    <div class="flex justify-around">
+      <h2 class="border p-4 w-full text-center" >Instrtuctions</h2>
+      <h2 class="border p-4 w-full text-center" >Enregistrement</h2>
+    </div>
+    <div>
+      <ul>
+        <li v-for="instruction in exercice.instructions" class="my-4 list-disc">
+          {{ instruction }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Exercise } from "@/stores/bodyParts";
+import { UnwrapRef } from "vue";
+
+const props = defineProps<{
+  exercice: UnwrapRef<Exercise>;
+}>();
+
+console.log("ExerciseItemPanel props", props);
+</script>
+
+<style scoped></style>
--- src/components/ExerciseItemPreview.vue
+++ src/components/ExerciseItemPreview.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="bg-white rounded-full">
-    <div class="flex justify-between items-center p-4 m-4 ">
+    <div class="flex justify-between items-center p-4 m-4">
       <img class="h-12" :src="exercice.gifUrl" alt="" />
       <span>{{ exercice.name }} </span>
-      <button>Save</button>
+      <button @click="togglePanel = !togglePanel">Save</button>
     </div>
   </div>
+  {{ togglePanel }}
+  <div v-if="togglePanel"><ExerciseItemPanel :exercice="exercice"/></div>
 </template>
 
 <script setup lang="ts">
 import { Exercise } from "@/stores/bodyParts";
-import { UnwrapRef } from "vue";
+import { UnwrapRef, ref } from "vue";
+import ExerciseItemPanel from "./ExerciseItemPanel.vue";
 
 const props = defineProps<{
   exercice: UnwrapRef<Exercise>;
 }>();
 
+const togglePanel = ref(false);
 </script>
 
 <style scoped></style>

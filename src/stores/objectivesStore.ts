adddba6018af35d858d8ce5ac9f989b48b7397db
Antony <antony.jacques-ext@gl-events.com>
2024-12-28 11:34:31 +0100

    store getter with params

--- src/stores/objectivesStore.ts
+++ src/stores/objectivesStore.ts
@@ -1,29 +1,70 @@
 import { defineStore } from "pinia";
 
-export const useObjectivesStore = defineStore('objectives', {
-  state: () => ({
+interface User {
+  id: number;
+  name: string;
+}
+
+interface ObjectivesState {
+  userInfo: {
+    name: string;
+  };
+  objectives: {
+    frequency: number;
+  };
+  currentStep: number;
+  users: User[];
+}
+
+export const useObjectivesStore = defineStore("objectives", {
+  state: (): ObjectivesState => ({
     userInfo: {
       name: "toto",
     },
     objectives: {
-      frequency: 1
+      frequency: 1,
     },
-    currentStep: 1
+    currentStep: 1,
+    users: [
+      {
+        id: 1,
+        name: "Jean",
+      },
+      {
+        id: 2,
+        name: "Paul",
+      },
+      {
+        id: 3,
+        name: "Quentin",
+      },
+    ],
   }),
 
-  actions: {
-    sayHello(){
-      alert(this.userInfo.name)
+  getters: {
+    getCurrentStep(state: ObjectivesState) {
+      return state.currentStep;
     },
 
-    editUserName(newName:string){
-      this.userInfo.name=newName
+    getUserById(state: ObjectivesState) {
+      return (userId: number) => {
+        console.log(state.users.find((user) => user.id === userId))
+        return state.users.find((user) => user.id === userId);
+      };
+    },
+  },
+
+  actions: {
+    sayHello() {
+      alert(this.userInfo.name);
+    },
+
+    editUserName(newName: string) {
+      this.userInfo.name = newName;
     },
 
     editObjectivesFrequency(newFrequency: number) {
-      this.objectives.frequency = newFrequency
+      this.objectives.frequency = newFrequency;
     },
-
-  }
-
+  },
 });
--- src/views/ObjectivesView.vue
+++ src/views/ObjectivesView.vue
@@ -3,7 +3,7 @@
     <h1>Objectives</h1>
     <h2>Bonjour {{ store.userInfo.name }}</h2>
     <h2>Frequency {{ store.objectives.frequency }}</h2>
-    <h2>Step {{ store.currentStep }}</h2>
+    <h2>Step getter {{ store.getCurrentStep }}</h2>
 
     <div v-if="formItems">
       <div v-for="formItem in formItems" :key="formItem.stepNumber">
@@ -13,13 +13,26 @@
         />
       </div>
 
-      <button @click="store.currentStep--" v-show="store.currentStep != 1" class="border p-4">Prev</button>
-      <button @click="store.currentStep++" class="border p-4" v-show="store.currentStep !== formItems.length">
+      <button
+        @click="store.currentStep--"
+        v-show="store.currentStep != 1"
+        class="border p-4"
+      >
+        Prev
+      </button>
+      <button
+        @click="store.currentStep++"
+        class="border p-4"
+        v-show="store.currentStep !== formItems.length"
+      >
         Next
       </button>
       <button @click="store.editUserInfos({ name: name })" class="border p-4">
         edit
       </button>
+      <button @click="findUser(1)" class="border p-4">user 1</button>
+      <button @click="findUser(2)" class="border p-4">user 2</button>
+      <button @click="store.getUserById(3)" class="border p-4">user 3</button>
     </div>
   </div>
 </template>
@@ -30,4 +43,6 @@ import FormStep from "@/components/Objectives/FormStep";
 import { useObjectivesStore } from "@/stores/objectivesStore";
 
 const store = useObjectivesStore();
+
+const findUser = store.getUserById;
 </script>
